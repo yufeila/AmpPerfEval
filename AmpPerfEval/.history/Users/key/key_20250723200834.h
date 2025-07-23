a6af93674d37773e95cc00df094b31a36e82cdda
yyf <17786321727@163.com>
2025-07-23 22:16:54 +0800

    增加了故障检测

---
+++ AmpPerfEval/.history/Users/key/key_20250723200834.h
@@ -0,0 +1,39 @@
+/*
+ * @Author: yyf 17786321727@163.com
+ * @Date: 2025-07-07 13:03:52
+ * @LastEditors: yyf 17786321727@163.com
+ * @LastEditTime: 2025-07-23 20:08:30
+ * @FilePath: /AmpPerfEval/Users/key/key.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#ifndef __KEY_H
+#define __KEY_H
+
+#include "stm32f4xx_hal.h"
+
+#define KEY0_PIN GPIO_PIN_8
+#define KEY0_PORT GPIOB
+
+#define KEY1_PIN GPIO_PIN_9
+#define KEY1_PORT GPIOB
+
+#define RELAY_PIN GPIO_PIN_12
+#define RELAY_PORT GPIOB
+
+#define KEY_UP_PIN GPIO_PIN_0
+#define 
+
+// 系统状态定义
+#define BASIC_MEASUREMENT_STATE           1
+#define SWEEP_FREQ_RESPONSE_STATE         2
+
+// 外部变量声明
+extern uint8_t basic_measurement_flag;      // 基本测量标志位
+extern uint8_t sweep_freq_response_flag;    // 扫频响应测量标志位
+extern uint8_t measure_r_out_flag;          // 输出阻抗测量标志位
+extern uint8_t current_system_state;        // 当前系统状态
+
+// 函数声明
+void KEY_Scan(void);
+
+#endif /* key.h */
---
+++ AmpPerfEval/.history/Users/key/key_20250723201016.c
@@ -0,0 +1,72 @@
+#include "key.h"
+
+// 标志位变量定义
+uint8_t basic_measurement_flag = 1;      // 基本测量标志位，初始为1
+uint8_t sweep_freq_response_flag = 0;    // 扫频响应测量标志位
+uint8_t measure_r_out_flag = 0;          // 输出阻抗测量标志位
+uint8_t current_system_state = BASIC_MEASUREMENT_STATE;  // 当前系统状态
+
+#define KEY_DEBOUNCE_DELAY 10 // ms
+
+static uint8_t key0_last_state = 1; // 默认按键未按下
+static uint8_t key1_last_state = 1;
+
+void Key_Init(void)
+{
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    GPIO_InitStruct.Pin = KEY_UP_PIN;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(KEY_UP_PORT, &GPIO_InitStruct);
+}
+
+
+void KEY_Scan(void)
+{
+    uint8_t key0_state = HAL_GPIO_ReadPin(KEY0_PORT, KEY0_PIN);
+    uint8_t key1_state = HAL_GPIO_ReadPin(KEY1_PORT, KEY1_PIN);
+
+    static uint32_t last_tick0 = 0;
+    static uint32_t last_tick1 = 0;
+
+    // ---- KEY0 检测与消抖 ----
+    if (key0_state == GPIO_PIN_RESET && key0_last_state == GPIO_PIN_SET) // 按下
+    {
+        if ((HAL_GetTick() - last_tick0) > KEY_DEBOUNCE_DELAY)
+        {
+            last_tick0 = HAL_GetTick();
+
+            if (current_system_state == BASIC_MEASUREMENT_STATE)
+            {
+                // 在基本测量模式下，KEY0用于触发输出阻抗测量
+                measure_r_out_flag = 1;
+            }
+            else
+            {
+                // 在其他模式下，KEY0切换到基本测量模式
+                basic_measurement_flag = 1;
+                sweep_freq_response_flag = 0;
+                measure_r_out_flag = 0;
+                current_system_state = BASIC_MEASUREMENT_STATE;
+            }
+        }
+    }
+    key0_last_state = key0_state;
+
+    // ---- KEY1 检测与消抗 ----
+    if (key1_state == GPIO_PIN_RESET && key1_last_state == GPIO_PIN_SET)
+    {
+        if ((HAL_GetTick() - last_tick1) > KEY_DEBOUNCE_DELAY)
+        {
+            last_tick1 = HAL_GetTick();
+
+            // KEY1: 切换到扫频响应测量模式
+            sweep_freq_response_flag = 1;
+            basic_measurement_flag = 0;
+            measure_r_out_flag = 0;
+            current_system_state = SWEEP_FREQ_RESPONSE_STATE;
+        }
+    }
+    key1_last_state = key1_state;
+}
